edfed5e9c101570b7509e98a3a623d5e90c8709c
Aleksandr Parmuzin <alexandr.parmuzin1@gamil.com>
2021-08-10 22:21:52 +0300

    added useState. Made images move after using SELECT button.

--- src/Components/Game/Game.jsx
+++ src/Components/Game/Game.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Game.module.css'
 
 const Game = () => {
 
+    const [hidden, setHidden] = useState(true);
+
+    function hide() {
+        setHidden();
+    }
+
+
+
     return (
         <div className={classes.game}>
-            <h3 style={{ textAlign: "center", paddingBottom: "10px", color: "#cd774c" }}>Chosen !</h3>
+            <h3 hidden={hidden} style={{ textAlign: "center", paddingBottom: "10px", color: "#cd774c" }}>Chosen !</h3>
             <img src="#" width="200px" height="300px"></img>
             <br></br>
             <p className={classes.game__title}>Long Long Long Long Game name</p>
-            <button className={classes.button}>Select</button>
+            <button onClick={hide} className={classes.button}>Select</button>
         </div>
     );
 };
